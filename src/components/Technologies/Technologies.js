aae22dfd8f6553607e8f62de9acb280c2a47fcef
DaveToth77 <85111342+DaveToth77@users.noreply.github.com>
2021-11-12 09:25:19 -0500

    added technologies structure/style

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -4,9 +4,43 @@ import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
-);
+<Section id='tech'>
+  <SectionDivider />
+  <br />
+  <SectionTitle>Technologies</SectionTitle>
+  <SectionText>
+    I've worked with a wide range of technologies in the web development world.
+    From Front-end to Back-end
+  </SectionText>
+  <List>
+    <ListItem>
+      <DiReact size='3rem' />
+      <listContainer>
+        <ListTitle>Front-End</ListTitle>
+        <ListParagraph></ListParagraph>
+          Experience with <br />
+          React.js <br />
+          Javascript <br />
+          HTML <br />
+          CSS <br />
+        </ListParagraph>
+      </listContainer>
+    </ListItem>
+    <ListItem>
+      <DiFirebase size='3rem' />
+      <listContainer>
+        <ListTitle>Back-End</ListTitle>
+        <ListParagraph>
+          Experience with <br />
+          Node.js <br />
+          MySql <br />
+          Javascript <br />
+
+        </ListParagraph>
+      </listContainer>
+    </ListItem>
+
+  </List>
+</Section>);
 
 export default Technologies;
