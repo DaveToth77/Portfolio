0061147428c62fe88c4f8a1f64217ae1170c67dc
DaveToth77 <85111342+DaveToth77@users.noreply.github.com>
2021-11-12 08:40:59 -0500

    added hero components/style

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,19 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hello <br />
+        My Name is Dave
+      </SectionTitle>
+      <SectionText>
+      I am a Full Stack Web Developer that has skills in both front and back end applications. I enjoy using innovative techniques as well as a strong attention to detail to get a project done with the clients expectations not only met, but exceeded. I seek a role as a web developer at a company where I can learn, grow, and continue to master the craft. 
+      </SectionText>
+      <Button onClick={() => window.location = 'mailto:dtoth77@gmail.com'}>Contact Me</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
