6ad685b12c1204845e9f20c86c96b2b64f7f50dc
DaveToth77 <85111342+DaveToth77@users.noreply.github.com>
2021-12-09 20:47:09 -0500

    updated technologies

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -3,47 +3,47 @@ import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
-const Technologies = () =>  (
-<Section id='tech'>
-  <SectionDivider />
-  <br />
-  <SectionTitle>Technologies</SectionTitle>
-  <SectionText>
-    I've worked with a wide range of technologies in the web development world.
-    From Front-end to Back-end
-  </SectionText>
-  <List>
-    <ListItem>
-      <DiReact size='3rem' />
-      <ListContainer>
-        <ListTitle>Front-End</ListTitle>
-        <ListParagraph>
-          Experience with <br />
-          React.js <br />
-          Javascript <br />
-          HTML <br />
-          CSS <br />
-          Bootstrap<br />
-        </ListParagraph>
-      </ListContainer>
-    </ListItem>
-    <ListItem>
-      <DiFirebase size='3rem' />
-      <listContainer>
-        <ListTitle>Back-End</ListTitle>
-        <ListParagraph>
-          Experience with <br />
-          Node.js <br />
-          MySql <br />
-          Javascript <br />
-          MongoDB <br />
-          Express <br />
-
-        </ListParagraph>
-      </listContainer>
-    </ListItem>
-
-  </List>
-</Section>);
+const Technologies = () => (
+  <Section id="tech">
+    <SectionDivider />
+    <br />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      I've worked with a wide range of technologies in the web development
+      world. From Front-end to Back-end
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiReact size="3rem" />
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+            React.js <br />
+            Javascript <br />
+            HTML <br />
+            CSS <br />
+            Styled Components <br />
+            Bootstrap
+            <br />
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiFirebase size="3rem" />
+        <listContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph>
+            Node.js <br />
+            GraphQL <br />
+            MySql <br />
+            Javascript <br />
+            MongoDB <br />
+            Express <br />
+          </ListParagraph>
+        </listContainer>
+      </ListItem>
+    </List>
+  </Section>
+);
 
 export default Technologies;
